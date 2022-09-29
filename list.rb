937106556e8fabefda3a824b20a1b2173afca759
Collins <mussocollins@gmail.com>
2022-09-29 17:29:40 +0300

    Add class MyList

--- list.rb
+++ list.rb
@@ -1,1 +1,14 @@
 require_relative 'enumerable'
+class MyList
+    include Enumerable
+    def initialize(*args)
+      @list = args
+    end
+  
+    def each
+      @list.each { |item| yield item if block_given? }
+    end
+  end
+  
+  list = MyList.new(1, 2, 3, 4)
+  list.each { |element| puts element }
